b1b4c4e02a4e1d7cbcbd9c2526d86ad439fe813b
”HiroshiUnno” <u.hiroshi19920103@gmail.com>
2020-07-23 17:00:56 +0900

    first commit

---
+++ 03kadai.php
@@ -0,0 +1,36 @@
+<?php
+//課題1
+$name = "HiroshiUnno";
+if ($name === "HiroshiUnno") {
+  echo "私は $name です";
+} else {
+  echo"あなたの名前ではありません";
+}
+
+//課題2
+$total = 0;
+echo $total;
+
+for ($idea = 0; $i <= 10000; $i++) {
+  $total += $i;
+}
+echo $total;
+
+//課題3
+$fruits = array("apple", "mango", "banana", "lemon", "melon");
+
+foreach($fruits as $fruit) {
+  echo "要素は" . $fruit;
+  echo "\n";
+}
+
+//課題4
+$start = 1;
+$end = 100;
+
+for($i = $start; $i <= $end; $i++) {
+  if(($i % 5) == 0){
+    echo $i;
+  }
+}
+?>
